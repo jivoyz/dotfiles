07ea7f3fcf14be03ea6deb271c6b0a3f8c4430fc
jIVOYz <ilaprohorov51@gmail.com>
2024-09-29 15:02:57 +0300

    added tmux-sessionizer & small fixes

--- Config/.config/fish/config.fish
+++ Config/.config/fish/config.fish
@@ -3,6 +3,8 @@ set -g fish_greeting
 if status is-interactive
     starship init fish | source
     fish_vi_key_bindings
+    bind -M insert \cf "sh ~/.config/hypr/scripts/tmux-sessionizer.sh"
+    bind -M insert \cn "sh ~/.config/hypr/scripts/search-notes.sh"
 
     alias vadim='$HOME/.config/hypr/scripts/vadim.sh'
     alias ls="exa --icons"
@@ -18,9 +20,19 @@ if status is-interactive
     alias gck="git checkout"
 
     alias vim="nvim"
-    alias zj="zellij"
 
     set -gx EDITOR nvim
-    set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+    # set -Ux FZF_DEFAULT_OPTS "
+    # --color=fg:#908caa,bg:#191724,hl:#ebbcba
+    # --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+    # --color=border:#403d52,header:#31748f,gutter:#191724
+    # --color=spinner:#f6c177,info:#9ccfd8
+    # --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+  set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+  --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+  --color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+  --color=selected-bg:#bcc0cc \
+  --multi"
 
 end
