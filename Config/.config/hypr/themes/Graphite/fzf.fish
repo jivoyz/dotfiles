3bd6bb2e5d32c89cc3c73ea1dc9895156a6808cb
jIVOYz <prokhorov_ilya@proton.me>
2025-01-17 15:20:10 +0200

    Add Graphite theme

---
+++ Config/.config/hypr/themes/Graphite/fzf.fish
@@ -0,0 +1,6 @@
+set -x FZF_DEFAULT_OPTS "\
+--color=bg+:#3c3b3e,bg:#161617,spinner:#ea83a5,hl:#ea83a5 \
+--color=fg:#e3e2e5,header:#e29eca,info:#ea83a5,pointer:#ea83a5 \
+--color=marker:#3c3b3e,fg+:#e3e2e5,prompt:#ea83a5,hl+:#ea83a5 \
+--color=selected-bg:#3c3b3e \
+--multi"
