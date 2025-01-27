1c45486b75a9d793ab94c601e3b956cfb5734c2c
jIVOYz <prokhorov_ilya@proton.me>
2025-01-27 18:06:19 +0200

    fix dracula tmux theme & some tweaks

--- Config/.config/fish/fzf.fish
+++ Config/.config/fish/fzf.fish
@@ -1,6 +1,2 @@
-set -x FZF_DEFAULT_OPTS "\
---color=bg+:#3c3b3e,bg:#161617,spinner:#ea83a5,hl:#ea83a5 \
---color=fg:#e3e2e5,header:#e29eca,info:#ea83a5,pointer:#ea83a5 \
---color=marker:#3c3b3e,fg+:#e3e2e5,prompt:#ea83a5,hl+:#ea83a5 \
---color=selected-bg:#3c3b3e \
---multi"
+set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
