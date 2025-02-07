74d420a4429bb9bcbf5f96aacb3b84360d8f1feb
jIVOYz <prokhorov_ilya@proton.me>
2025-02-07 22:36:16 +0200

    nothing

--- Config/.config/fish/fzf.fish
+++ Config/.config/fish/fzf.fish
@@ -1,6 +1,6 @@
 set -x FZF_DEFAULT_OPTS "\
---color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
---color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
---color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
---color=selected-bg:#bcc0cc \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#89b4fa,hl:#eba0ac \
+--color=fg:#cdd6f4,header:#f38ba8,info:#89b4fa,pointer:#eba0ac \
+--color=marker:#6c7086,fg+:#cdd6f4,prompt:#89b4fa,hl+:#eba0ac \
+--color=selected-bg:#6c7086 \
 --multi"
