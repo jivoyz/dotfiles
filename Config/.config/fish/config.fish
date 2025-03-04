c3468b10268d1972ddf28ab7dfe1bd376f06f8c5
jIVOYz <prokhorov_ilya@proton.me>
2025-03-04 20:35:47 +0200

    many changes

--- Config/.config/fish/config.fish
+++ Config/.config/fish/config.fish
@@ -11,16 +11,17 @@ set -g fish_greeting
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
+    fish_add_path $HOME/.local/bin
     source ~/.config/fish/fzf.fish
 
     # Set up fzf key bindings
     fzf --fish | source
+    set -x FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git --exclude node_modules"
 
     fish_vi_key_bindings
     bind -M insert \cf "sh ~/.config/hypr/scripts/tmux-sessionizer.sh"
     bind -M insert \cn "sh ~/.config/hypr/scripts/search-notes.sh"
 
-    alias vadim='$HOME/.config/hypr/scripts/vadim.sh'
     alias ls="exa --icons"
     alias la="exa --icons -a"
     alias lla="exa --icons -l -a"
@@ -28,10 +29,6 @@ if status is-interactive
     alias ll="exa -l -g --icons"
 
     alias g="git"
-    alias gc="git clone"
-    alias gcm="git commit"
-    alias gst="git status"
-    alias gck="git checkout"
 
     alias vim="nvim"
 
