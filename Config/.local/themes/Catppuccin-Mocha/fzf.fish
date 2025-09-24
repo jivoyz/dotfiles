ee679e24dfd44aaa69b712c5e9dbe35912273d7e
jivoyz <prokhorov_ilya@proton.me>
2025-09-24 12:10:50 +0300

    delete unused themes

--- Config/.local/themes/Catppuccin-Mocha/fzf.fish
+++
@@ -1,6 +0,0 @@
-set -x FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#89b4fa,hl:#eba0ac \
---color=fg:#cdd6f4,header:#f38ba8,info:#89b4fa,pointer:#eba0ac \
---color=marker:#6c7086,fg+:#cdd6f4,prompt:#89b4fa,hl+:#eba0ac \
---color=selected-bg:#6c7086 \
---multi"
